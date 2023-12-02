69d932c51741441a2d77983acdd7b09545b06335
Knyazew36 <knyazew.sv@gmail.com>
2023-12-02 21:19:39 +0300

    1

--- sender.php
+++ sender.php
@@ -1,22 +1,38 @@
 <?php
-    $name = $_POST['name'];
-	$phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
 
-	$to = "knyazew.sv@gmail.com"; 
-	$date = date ("d.m.Y"); 
-	$time = date ("h:i");
-	$from = $email;
-	$subject = "Заявка c сайта";
+$mailSender = 'test@семеннойзавод.рф';
+$mailRecipient = 'knyazew.sv@gmail.com';
+$boundary = uniqid('np');
+$headers = 'From: ' . $mailSender  . PHP_EOL .
+             'Reply-To: ' . $mailSender  . PHP_EOL .
+             'MIME-Version: 1.0' . PHP_EOL .
+             "Content-Type: multipart/alternative;boundary=" . $boundary . PHP_EOL;
 
-	
-	$msg="
-    Имя: $name /n
-    Телефон: $phone /n
-    Почта: $email /n
-    Текст: $text"; 	
-	mail($to, $subject, $msg, "From: $to ");
+// Создаем текст сообщения, включающий все предыдущие переменные
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$comment = $_POST['message'];
 
+$messageBody = "This is a MIME encoded message.";
+$messageBody .= "\r\n\r\n--" . $boundary . "\r\n";
+$messageBody .= "Content-type: text/plain;charset=utf-8\r\n\r\n";
+
+//Plain text body
+$messageBody .= "Hello,\nThis is a text email, the text/plain version.\n\n";
+$messageBody .= "Имя: $name\n";
+$messageBody .= "Телефон: $phone\n";
+$messageBody .= "Email: $email\n";
+$messageBody .= "Сообщение: $comment\n";
+$messageBody .= "\r\n\r\n--" . $boundary . "--";
+
+if (!empty($mailRecipient) && isValidEmail($mailRecipient)) {
+    if (mail($mailRecipient, "TEST mail", $messageBody, $headers, '-f '.$mailSender)) {
+        echo "php_mail: Отправлено, получатель ", $mailRecipient;
+    } else {
+        echo "php_mail: Ошибка, проверьте правильность введенных данных";
+    }
+} else {
+    echo "php_mail: Некорректный адрес получателя";
+}
 ?>
-
